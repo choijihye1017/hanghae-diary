1144375034210d191056c1e599b8af2921412084
최정운 <>
2023-03-31 00:13:21 +0900

    404 Error 제거 및 Category 추가

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import certifi
 
 ca=certifi.where()
 
-client = MongoClient("mongodb+srv://test:test@cluster0.15fhovx.mongodb.net/test", tlsCAFile=ca)
-db = client.dbsparta_plus_week4
+client = MongoClient("mongodb+srv://sparta:test@cluster0.inqbmby.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+db = client.hanghae
 
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
@@ -51,6 +51,9 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/write')
+def gotopage():
+    return render_template('writepage.html')
 
 #################################
 ##  로그인을 위한 API            ##
